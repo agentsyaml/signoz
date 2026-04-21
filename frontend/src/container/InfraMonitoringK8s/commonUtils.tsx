c252fdd93950c93cbf73826127bd95fe259b812d
Vinícius Lourenço <vinicius@signoz.io>
2026-04-21 15:13:31 -0300

    feat(k8s-base-details): migrate logs/traces/events to query builder v5

--- frontend/src/container/InfraMonitoringK8s/commonUtils.tsx
+++ frontend/src/container/InfraMonitoringK8s/commonUtils.tsx
@@ -1,12 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
-import { useMemo } from 'react';
 import { Color } from '@signozhq/design-tokens';
 import { Progress, Table, Tooltip, Typography } from 'antd';
-import type { ColumnsType } from 'antd/lib/table';
-import { ResizeTable } from 'components/ResizeTable';
-import FieldRenderer from 'container/LogDetailedView/FieldRenderer';
-import { DataType } from 'container/LogDetailedView/TableView';
 import {
 	IBuilderQuery,
 	TagFilterItem,
@@ -129,51 +124,6 @@ export function EntityProgressBar({
 			/>
 			<Typography.Text style={{ fontSize: '10px' }}>{percentage}%</Typography.Text>
 		</div>
-	);
-}
-
-export function EventContents({
-	data,
-}: {
-	data: Record<string, string> | undefined;
-}): JSX.Element {
-	const tableData = useMemo(
-		() =>
-			data ? Object.keys(data).map((key) => ({ key, value: data[key] })) : [],
-		[data],
-	);
-
-	const columns: ColumnsType<DataType> = [
-		{
-			title: 'Key',
-			dataIndex: 'key',
-			key: 'key',
-			width: 50,
-			align: 'left',
-			className: 'attribute-pin value-field-container',
-			render: (field: string): JSX.Element => <FieldRenderer field={field} />,
-		},
-		{
-			title: 'Value',
-			dataIndex: 'value',
-			key: 'value',
-			width: 50,
-			align: 'left',
-			ellipsis: true,
-			className: 'attribute-name',
-			render: (field: string): JSX.Element => <FieldRenderer field={field} />,
-		},
-	];
-
-	return (
-		<ResizeTable
-			columns={columns}
-			tableLayout="fixed"
-			dataSource={tableData}
-			pagination={false}
-			showHeader={false}
-			className={styles.eventContentContainer}
-		/>
 	);
 }
 
